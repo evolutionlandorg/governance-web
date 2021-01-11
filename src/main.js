1e373cbe108d93257e6427c89323b499d6be8d12
WoeOm <1015996366@qq.com>
2021-01-11 16:11:27 +0800

    remove unused components

--- src/main.js
+++ src/main.js
@@ -9,14 +9,12 @@ import i18n from "./i18n";
 import {Web3Modal} from './components'
 
 import {
-  Pagination,
   Dialog,
   Autocomplete,
   Dropdown,
   DropdownMenu,
   DropdownItem,
   Menu,
-  Submenu,
   MenuItem,
   MenuItemGroup,
   Input,
@@ -25,8 +23,6 @@ import {
   RadioGroup,
   RadioButton,
   Checkbox,
-  CheckboxButton,
-  CheckboxGroup,
   Switch,
   Select,
   Option,
@@ -35,52 +31,25 @@ import {
   ButtonGroup,
   Table,
   TableColumn,
-  DatePicker,
-  TimeSelect,
-  TimePicker,
   Popover,
   Tooltip,
-  Breadcrumb,
-  BreadcrumbItem,
   Form,
   FormItem,
   Tabs,
   TabPane,
   Tag,
-  Tree,
   Alert,
-  Slider,
   Icon,
   Row,
   Col,
-  Upload,
-  Progress,
   Spinner,
-  Badge,
-  Card,
   Rate,
-  Steps,
-  Step,
-  Carousel,
-  CarouselItem,
-  Collapse,
-  CollapseItem,
-  Cascader,
-  ColorPicker,
-  Transfer,
   Container,
   Header,
-  Aside,
   Main,
   Footer,
-  Timeline,
-  TimelineItem,
   Link,
-  Divider,
   Image,
-  Calendar,
-  Backtop,
-  PageHeader,
   CascaderPanel,
   Loading,
   MessageBox,
@@ -97,14 +66,12 @@ import {
 } from "@/components/index"
 
 Vue.use(TableEmpty);
-Vue.use(Pagination);
 Vue.use(Dialog);
 Vue.use(Autocomplete);
 Vue.use(Dropdown);
 Vue.use(DropdownMenu);
 Vue.use(DropdownItem);
 Vue.use(Menu);
-Vue.use(Submenu);
 Vue.use(MenuItem);
 Vue.use(MenuItemGroup);
 Vue.use(Input);
@@ -113,8 +80,6 @@ Vue.use(Radio);
 Vue.use(RadioGroup);
 Vue.use(RadioButton);
 Vue.use(Checkbox);
-Vue.use(CheckboxButton);
-Vue.use(CheckboxGroup);
 Vue.use(Switch);
 Vue.use(Select);
 Vue.use(Option);
@@ -123,9 +88,6 @@ Vue.use(Button);
 Vue.use(ButtonGroup);
 Vue.use(Table);
 Vue.use(TableColumn);
-Vue.use(DatePicker);
-Vue.use(TimeSelect);
-Vue.use(TimePicker);
 Vue.use(Popover);
 Vue.use(Tooltip);
 Vue.use(Form);
@@ -134,34 +96,17 @@ Vue.use(Tabs);
 Vue.use(TabPane);
 Vue.use(Tag);
 Vue.use(Alert);
-Vue.use(Slider);
 Vue.use(Icon);
 Vue.use(Row);
 Vue.use(Col);
-Vue.use(Upload);
-Vue.use(Progress);
 Vue.use(Spinner);
-Vue.use(Badge);
-Vue.use(Card);
 Vue.use(Rate);
-Vue.use(Steps);
-Vue.use(Step);
-Vue.use(Carousel);
-Vue.use(CarouselItem);
-Vue.use(Collapse);
-Vue.use(CollapseItem);
-Vue.use(Cascader);
-Vue.use(ColorPicker);
-Vue.use(Transfer);
 Vue.use(Container);
 Vue.use(Header);
-Vue.use(Aside);
 Vue.use(Main);
 Vue.use(Footer);
 Vue.use(Link);
-Vue.use(Divider);
 Vue.use(Image);
-Vue.use(Backtop);
 Vue.use(CascaderPanel);
 Vue.use(Drawer);
 
