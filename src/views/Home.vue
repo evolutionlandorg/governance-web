19192606982bbe061bbe05836b554f3460baab81
WoeOm <1015996366@qq.com>
2021-01-15 18:25:37 +0800

    fix url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -51,7 +51,7 @@
                   <div class="asset-buttons">
                     <div class="item">
                       <p>{{$t('my land')}}: {{formatFixedDecimals(_evolutionTeller_get_value.balanceOfLandOwner)}}</p>
-                      <el-button class="fixed-button" size="small" type="primary" @click="openUrl('https://www.evolution.land.l2me.com/land/1?goUrl=%2Flandmarket%2Fsearch')">{{$t('buy')}}</el-button>
+                      <el-button class="fixed-button" size="small" type="primary" @click="openUrl('https://www.evolution.land/land/1?goUrl=%2Flandmarket%2Fsearch')">{{$t('buy')}}</el-button>
                     </div>
                   </div>
                 </div>
@@ -70,7 +70,7 @@
                   <div class="asset-buttons">
                     <div class="item">
                       <p>{{$t('my apostle')}}: {{formatFixedDecimals(_evolutionTeller_get_value.balanceOfApostleOwner)}}</p>
-                      <el-button class="fixed-button" size="small" type="primary" @click="openUrl('https://www.evolution.land.l2me.com/land/1?goUrl=%2Fapostle')">{{$t('buy')}}</el-button>
+                      <el-button class="fixed-button" size="small" type="primary" @click="openUrl('https://www.evolution.land/land/1?goUrl=%2Fapostle')">{{$t('buy')}}</el-button>
                     </div>
                   </div>
                 </div>
